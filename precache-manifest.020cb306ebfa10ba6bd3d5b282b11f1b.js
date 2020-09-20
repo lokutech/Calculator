777e6aff68f354d26ab8ab6bee8ea1654689d4ee
Loku <onatim@protonmail.com>
2020-09-20 22:09:23 +0300

    Updates

---
+++ precache-manifest.020cb306ebfa10ba6bd3d5b282b11f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c997770ab7217e4715fdaa261cbdf0",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "01a8344487f25083b71b",
+    "url": "/Calculator/static/css/main.b6ce7b43.chunk.css"
+  },
+  {
+    "revision": "bc894b9afd252b226875",
+    "url": "/Calculator/static/js/2.70fcda0c.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calculator/static/js/2.70fcda0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a8344487f25083b71b",
+    "url": "/Calculator/static/js/main.e04d837e.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
